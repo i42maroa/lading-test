8f141bc50cc6b89ee338875b2acf35923ac5bc8f
i42maroa <i42maroa@uco.es>
2021-04-07 13:28:27 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+      mode: 'fade'
+    })
+  });
+  $(document).ready(function(){
+    $('.slider2').bxSlider({
+      mode: 'fade'
+    })
+  });
+  $(document).ready(function(){
+    $('.slider3').bxSlider({
+      mode: 'fade'
+    })
+  });
